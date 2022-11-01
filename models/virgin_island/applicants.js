f9e52eb96ed9829aca1349304381615e0d2500ee
syedfakher27 <sfakher27@gmail.com>
2022-11-01 18:22:27 +0500

    application_table added

---
+++ models/virgin_island/applicants.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Applicant extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Applicant.init(
+        {
+            application_id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            file_id: DataTypes.STRING,
+            map_case_number: DataTypes.STRING,
+            applicant_name: DataTypes.STRING,
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            is_deleted: DataTypes.BOOLEAN
+        },
+        {
+            sequelize,
+            modelName: 'applicants',
+            schema: 'virgin_island',
+            createdAt: false,
+            updatedAt: false
+        });
+    return Applicant;
+};
