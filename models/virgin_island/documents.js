38a55223ba5cd0ccea4859e701f3f72dcd21c595
syedfakher27 <sfakher27@gmail.com>
2022-11-05 15:27:25 +0500

    documents updated

--- models/virgin_island/documents.js
+++ models/virgin_island/documents.js
@@ -13,44 +13,49 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     }
-    Document.init(
-        {
-            file_id: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                primaryKey: true,
+        Document.init(
+            {
+                file_id: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                   
+                },
+                case_no: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                },
+                submission_id: DataTypes.STRING,
+                document_name: DataTypes.STRING,
+                document_type: DataTypes.STRING,
+                status: DataTypes.STRING,
+                priority: DataTypes.STRING,
+                case_status: DataTypes.STRING,
+                case_category: DataTypes.STRING,
+                adjudicate_status:{ 
+                    type:DataTypes.STRING,
+                    defaultValue: 'Requires Review'
+                },
+                is_open: DataTypes.BOOLEAN,
+                created_at: {
+                    type: DataTypes.DATE
+                },
+                updated_at: {
+                    type: DataTypes.DATE
+                },
+                is_deleted: DataTypes.BOOLEAN
             },
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                autoIncrement: true
-            },
-            submission_id: DataTypes.STRING,
-            document_name: DataTypes.STRING,
-            document_type: DataTypes.STRING,
-            status: DataTypes.STRING,
-            priority: DataTypes.STRING,
-            case_status: DataTypes.STRING,
-            case_category: DataTypes.STRING,
-            adjudicate_status:{ 
-                type:DataTypes.STRING,
-                defaultValue: 'Requires Review'
-            },
-            is_open: DataTypes.BOOLEAN,
-            created_at: {
-                type: DataTypes.DATE
-            },
-            updated_at: {
-                type: DataTypes.DATE
-            },
-            is_deleted: DataTypes.BOOLEAN
-        },
-        {
-            sequelize,
-            modelName: 'documents',
-            schema: 'virgin_island',
-            createdAt: false,
-            updatedAt: false
-        });
+            {
+                sequelize,
+                modelName: 'documents',
+                schema: 'virgin_island',
+                createdAt: false,
+                updatedAt: false
+            });
+        
+    
+
+
     return Document;
 };
