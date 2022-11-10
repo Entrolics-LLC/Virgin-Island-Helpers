b8c0dd33938e43e482e56bbf97bf85ee0e688d90
m-yasir-khan <yasir.khan@mach3bi.com>
2022-11-10 12:19:30 +0500

    Create case_docs.js

---
+++ models/virgin_island/case_docs.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class CaseDocs extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    CaseDocs.init(
+        {
+            file_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+
+            },
+            document_name: DataTypes.STRING,
+            document_type: DataTypes.STRING,
+            category_type: DataTypes.STRING,
+            status: DataTypes.STRING,
+            // priority: DataTypes.STRING,
+            // case_status: DataTypes.STRING,
+            // case_category: DataTypes.STRING,
+            // category_confidence: DataTypes.FLOAT,
+            // adjudicate_status: {
+            //     type: DataTypes.STRING,
+            //     defaultValue: 'Requires Review'
+            // },
+            is_open: { type: DataTypes.BOOLEAN, defaultValue: false },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            is_deleted: DataTypes.BOOLEAN
+        },
+        {
+            sequelize,
+            modelName: 'case_docs',
+            schema: 'virgin_island',
+            createdAt: false,
+            updatedAt: false
+        });
+
+
+
+
+    return CaseDocs;
+};
