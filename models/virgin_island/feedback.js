e6742fd8537d0f7dc837bd9b37cd925f04d69c58
syedfakher27 <sfakher27@gmail.com>
2022-11-14 15:07:19 +0500

    feedbacks added

---
+++ models/virgin_island/feedback.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Feedback extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+        Feedback.init(
+            {
+                id: {
+                    type: DataTypes.STRING,
+                    primaryKey: true,
+                    allowNull: false,
+                   
+                },
+                feedback: DataTypes.STRING(1000),
+                user_id: DataTypes.STRING,
+                rating: DataTypes.FLOAT,
+                created_at: {
+                    type: DataTypes.DATE
+                },
+                is_deleted: DataTypes.BOOLEAN
+            },
+            {
+                sequelize,
+                modelName: 'feedbacks',
+                schema: 'virgin_island',
+                createdAt: false,
+                updatedAt: false
+            });
+        
+    
+
+
+    return Feedback;
+};
