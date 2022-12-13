0116de43a42b172e1ab21e1c6f51a15f1c237d65
m-yasir-khan <yasir.khan@mach3bi.com>
2022-12-13 11:29:58 +0500

    Add Tables

---
+++ models/irs/pdf_pages.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class PdfPages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PdfPages.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    file_name: {
+      type: DataTypes.STRING
+    },
+    dimensions: DataTypes.STRING,
+    pageNumber: DataTypes.INTEGER,
+    paragraphs: 'CHARACTER VARYING(1000000)'
+  }, {
+    sequelize,
+    modelName: 'pdf_pages',
+    schema: 'irs',
+    createdAt: false,
+    updatedAt: false
+  });
+  return PdfPages;
+};
---
+++ models/virgin_island/pdf_document.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PdfDocument extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PdfDocument.init({
+    file_name: {
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    pages_count: DataTypes.INTEGER,
+    entities_count: DataTypes.INTEGER,
+    text: 'CHARACTER VARYING(1000000)',
+    schema_id: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'pdf_document',
+    schema: 'virgin_island',
+    createdAt: false,
+    updatedAt: false
+  });
+  return PdfDocument;
+};
